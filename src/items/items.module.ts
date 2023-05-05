1ccb6696ae0037acf2f61f4bc192366efc66ea2c
aimeliala <idstack@outlook.co.id>
2023-05-06 00:45:45 +0700

    melihat hasil entitas di dalam database

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -1,9 +1,12 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { ItemsController } from './items.controller';
 import { ItemsService } from './items.service';
+import { Item } from './item.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Item])],
   controllers: [ItemsController],
-  providers: [ItemsService]
+  providers: [ItemsService],
 })
 export class ItemsModule {}
