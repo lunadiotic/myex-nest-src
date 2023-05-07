8ef26d857961cedf21ad861de7bc2dea449c0e2d
aimeliala <idstack@outlook.co.id>
2023-05-07 17:03:38 +0700

    membuat query untuk mendapatkan data user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,8 +9,20 @@ export class UsersService {
     @InjectRepository(User) private usersRepository: Repository<User>,
   ) {}
 
+  find() {
+    return this.usersRepository.find();
+  }
+
   create(name: string, email: string, password: string) {
     const user = this.usersRepository.create({ name, email, password });
     return this.usersRepository.save(user);
   }
+
+  findOneBy(id: number) {
+    return this.usersRepository.findOneBy({ id });
+  }
+
+  update() {}
+
+  remove() {}
 }
