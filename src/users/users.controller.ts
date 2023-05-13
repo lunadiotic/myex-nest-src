627516f2980e5fb7aac853c34e3f3c7c413c7a7c
aimeliala <idstack@outlook.co.id>
2023-05-13 16:43:53 +0700

    implementasi query update di controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,12 +3,14 @@ import {
   Controller,
   Post,
   Get,
+  Patch,
   Delete,
   Param,
   Query,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -27,6 +29,11 @@ export class UsersController {
   @Get('/:id')
   findUser(@Param('id') id: string) {
     return this.usersService.findOneBy(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
   }
 
   @Delete('/:id')
