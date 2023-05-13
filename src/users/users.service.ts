0cd405108c259c71e6841ea50c377016212bad9c
aimeliala <idstack@outlook.co.id>
2023-05-13 17:01:28 +0700

    menggunakan notfound exception

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -22,24 +22,22 @@ export class UsersService {
     return this.usersRepository.save(user);
   }
 
-  findOneBy(id: number) {
-    return this.usersRepository.findOneBy({ id });
+  async findOneBy(id: number) {
+    const user = await this.usersRepository.findOneBy({ id });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
   }
 
   async update(id: number, attrs: Partial<User>) {
     const user = await this.findOneBy(id);
-    if (!user) {
-      throw new Error('User not found');
-    }
     Object.assign(user, attrs);
     return this.usersRepository.save(user);
   }
 
   async remove(id: number) {
     const user = await this.findOneBy(id);
-    if (!user) {
-      throw new Error('User not found');
-    }
     return this.usersRepository.remove(user);
   }
 }
