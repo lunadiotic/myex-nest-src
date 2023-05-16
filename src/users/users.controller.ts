e1b0d49d7f71796eeb7cd9e7a412d80673d9f63a
aimeliala <idstack@outlook.co.id>
2023-05-16 17:20:03 +0700

    membuat dto interceptor dinamis

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,12 +8,12 @@ import {
   Param,
   Query,
   UseInterceptors,
-  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -29,7 +29,7 @@ export class UsersController {
     return this.usersService.create(body.name, body.email, body.password);
   }
 
-  @UseInterceptors(SerializeInterceptor)
+  @UseInterceptors(new SerializeInterceptor(UserDto))
   @Get('/:id')
   findUser(@Param('id') id: string) {
     console.log('handler is running');
