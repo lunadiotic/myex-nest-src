1c94f7fde52863c5dd981a069945787cd2979ccd
aimeliala <idstack@outlook.co.id>
2023-05-19 23:22:18 +0700

    cara membuat salt dan hash pada password

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,15 +1,22 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
-  async register(email: string, password: string) {
+  async register(name: string, email: string, password: string) {
     const users = await this.usersService.find(email);
     if (users.length) {
       throw new BadRequestException('User already exists');
     }
     // Hash password
+    const salt = randomBytes(8).toString('hex');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    const result = salt + '.' + hash.toString('hex');
 
     // Save user
   }
