4d49d34032b3ca0a274ecf24b1507d2d9d539ef2
aimeliala <idstack@outlook.co.id>
2023-05-20 09:10:17 +0700

    membuat service untuk proses login

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { randomBytes, scrypt as _scrypt } from 'crypto';
 import { promisify } from 'util';
@@ -20,6 +24,22 @@ export class AuthService {
 
     // Save user
     const user = await this.usersService.create(name, email, hashedPassword);
+
+    return user;
+  }
+
+  async login(email: string, password: string) {
+    const [user] = await this.usersService.find(email);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    const [salt, storedHash] = user.password.split('.');
+
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    if (storedHash !== hash.toString('hex')) {
+      throw new BadRequestException('Bad password');
+    }
 
     return user;
   }
