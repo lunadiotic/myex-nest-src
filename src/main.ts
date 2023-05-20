1a9cd33686eb93baa18f93bcc271950ef99b9536
aimeliala <idstack@outlook.co.id>
2023-05-20 11:45:27 +0700

    setup cookie session

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import cookieSession from 'cookie-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSession({ keys: ['r4nd0m5tRin6'] }));
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
