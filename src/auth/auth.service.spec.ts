b6178fc867cd5a595a43c9e5a82ac8e3391ec135
aimeliala <idstack@outlook.co.id>
2023-05-24 11:30:23 +0700

    setup unit test di auth service

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,29 @@
+import { UsersService } from '../users/users.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { User } from 'src/users/user.entity';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
+    const fakeUsersService = {
+      find: () => {
+        return Promise.resolve([]);
+      },
+      create: (name: string, email: string, password: string) => {
+        return Promise.resolve({ id: 1, name, email, password } as User);
+      },
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
