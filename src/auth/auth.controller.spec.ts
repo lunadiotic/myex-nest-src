32f8056609be8dceb380616249c24a398e52db22
aimeliala <idstack@outlook.co.id>
2023-05-27 12:38:46 +0700

    membuat unit test untuk login auth controller

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { User } from '../users/user.entity';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -9,7 +10,13 @@ describe('AuthController', () => {
   beforeEach(async () => {
     fakeAuthService = {
       // register: (email: string, password: string) => {},
-      // login: (email: string, password: string) => {},
+      login: (email: string, password: string) => {
+        return Promise.resolve({
+          id: 1,
+          email,
+          password,
+        } as User);
+      },
     };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
@@ -26,5 +33,15 @@ describe('AuthController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return a user on login', async () => {
+    const session = { userId: -10 };
+    const user = await controller.login(
+      { email: 'email', password: 'password' },
+      session,
+    );
+    expect(user).toEqual({ id: 1, email: 'email', password: 'password' });
+    expect(session['userId']).toEqual(1);
   });
 });
