20a103e9eb72397ebcaee3b483bd002c0572e71e
aimeliala <idstack@outlook.co.id>
2023-05-29 15:29:20 +0700

    langkah awal membuat e2e test

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,35 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('Auth Controller (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('handles register (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/auth/register')
+      .send({
+        name: 'sakata',
+        email: 'kintoki3@mail.com',
+        password: 'test',
+      })
+      .expect(201)
+      .then(({ body }: request.Response) => {
+        expect(body).toMatchObject({
+          id: expect.any(String),
+          name: 'sakata',
+          email: 'kintoki1@mail.com',
+        });
+      });
+  });
+});
