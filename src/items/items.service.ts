f236ad9099cf80841ba168a19feae757c66ca6ce
aimeliala <idstack@outlook.co.id>
2023-06-03 11:53:15 +0700

    membuat service create item

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Item } from './item.entity';
+import { CreateItemDto } from './dtos/create-item.dto';
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+  constructor(
+    @InjectRepository(Item) private itemRepository: Repository<Item>,
+  ) {}
+
+  create(itemDto: CreateItemDto) {
+    const item = this.itemRepository.create(itemDto);
+    return this.itemRepository.save(item);
+  }
+}
