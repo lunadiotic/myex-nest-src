19f4dff3a773e9abc353d825f181acb7f058cb2a
aimeliala <idstack@outlook.co.id>
2023-06-04 11:24:21 +0700

    menyimpan data item beserta usernya

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -3,14 +3,16 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Item } from './item.entity';
 import { CreateItemDto } from './dtos/create-item.dto';
+import { User } from '../users/user.entity';
 @Injectable()
 export class ItemsService {
   constructor(
     @InjectRepository(Item) private itemRepository: Repository<Item>,
   ) {}
 
-  create(itemDto: CreateItemDto) {
+  create(itemDto: CreateItemDto, user: User) {
     const item = this.itemRepository.create(itemDto);
+    item.user = user;
     return this.itemRepository.save(item);
   }
 }
