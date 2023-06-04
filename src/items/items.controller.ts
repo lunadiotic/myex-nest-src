3a9b4aff90a37b33a26e183a2031776777bedc3b
aimeliala <idstack@outlook.co.id>
2023-06-04 08:40:34 +0700

    menyimpan data item beserta usernya

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { CreateItemDto } from './dtos/create-item.dto';
 import { ItemsService } from './items.service';
 import { AuthGuard } from '../guards/auth.guard';
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
+import { User } from '../users/user.entity';
 
 @Controller('items')
 export class ItemsController {
@@ -9,7 +11,7 @@ export class ItemsController {
 
   @Post
   @UseGuards(AuthGuard)
-  createItem(@Body() body: CreateItemDto) {
-    return this.itemService.create(body);
+  createItem(@Body() body: CreateItemDto, @CurrentUser() user: User) {
+    return this.itemService.create(body, user);
   }
 }
